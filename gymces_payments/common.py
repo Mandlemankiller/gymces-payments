2f0a59674bf4eec6a1a63f7a96b9a158fb5689d2
Mandlemankiller <mandlemankiller@email.cz>
2023-09-17 20:37:42 +0200

    Use formatted strings for logging

--- gymces_payments/common.py
+++ gymces_payments/common.py
@@ -1,11 +1,11 @@
 def error(msg: str) -> None:
-    print('\033[1;31m[ERROR]: ' + msg + '\033[0m')
+    print(f'\033[1;31m[ERROR]: {msg}\033[0m')
 
 
 def fatal(msg: str) -> None:
-    print('\033[1;31m[FATAL]: ' + msg + '\033[0m')
+    print(f'\033[1;31m[FATAL]: {msg}\033[0m')
     exit()
 
 
 def warning(msg: str) -> None:
-    print('\033[1;33m[WARNING]: ' + msg + '\033[0m')
+    print(f'\033[1;33m[WARNING]: {msg}\033[0m')
